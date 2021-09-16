366dc3ba17d7246ae709e7667400b6e463ed37e3
BastianCantero <90585029+BastianCantero@users.noreply.github.com>
2021-09-16 03:52:32 -0300

    tarea2

--- Tarea_2/app/src/main/java/com/bcantero/tarea_2/MainActivity.java
+++ Tarea_2/app/src/main/java/com/bcantero/tarea_2/MainActivity.java
@@ -3,25 +3,35 @@ package com.bcantero.tarea_2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView txtName, txtLastName;
+    private TextView txtName, txtLastName, ccc;
+    private CheckBox cbxJava, cbxPhp, cbxPython, cbxCPlusPlus, cbxCcharp;
     private String information;
+
+    private RadioGroup radioGroup1;
+
+
     String name;
+    String lastName;
+
+    String favoriteLanguaje;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        txtName = findViewById(R.id.txtName);
-        txtLastName = findViewById(R.id.txtLastName);
     }
 
-    void getInformation(){
-        //name = txtName.getText();
+    public void getInformation(View view){
+        name = String.valueOf(txtName.getText());
+        lastName = String.valueOf(txtLastName.getText());
+
     }
 
 }
