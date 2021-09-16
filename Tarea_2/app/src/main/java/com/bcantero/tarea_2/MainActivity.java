e33f81fb8bd0ac4be408a0b0dde14bdb50c4f097
BastianCantero <90585029+BastianCantero@users.noreply.github.com>
2021-09-16 00:42:17 -0300

    tarea2

--- Tarea_2/app/src/main/java/com/bcantero/tarea_2/MainActivity.java
+++ Tarea_2/app/src/main/java/com/bcantero/tarea_2/MainActivity.java
@@ -3,12 +3,25 @@ package com.bcantero.tarea_2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView txtName, txtLastName;
+    private String information;
+    String name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtName = findViewById(R.id.txtName);
+        txtLastName = findViewById(R.id.txtLastName);
     }
+
+    void getInformation(){
+        //name = txtName.getText();
+    }
+
 }
